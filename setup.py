e103b838718937f62587c53018f772189d76eef3
Martin Ahindura <martinahindura@gmail.com>
2020-03-29 00:48:51 +0300

    Add the python publishing set up files

---
+++ setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="function-ext-monitor",
+    version="0.0.1",
+    description="This package provides a decorator to wrap around a function so that a report is sent to external server every time function runs.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sopherapps/function-ext-monitor",
+    author="Martin Ahindura",
+    author_email="team.sopherapps@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    packages=find_packages(exclude=("test",)),
+    py_modules=['main'],
+    include_package_data=True,
+    install_requires=["requests"],
+    entry_points={
+    },
+)
