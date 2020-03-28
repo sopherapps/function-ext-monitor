2f4ea6ee7ea11465ca526bf5992dd1fae39966ad
Martin Ahindura <martinahindura@gmail.com>
2020-03-28 19:23:05 +0300

    Add tests

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import socket
 import json
 import requests
-from multiprocessing import Process
+import multiprocessing
+
+
+# from multiprocessing import Process
 
 
 def send_report(report_server_url, data_as_dict={}, headers={}):
@@ -9,14 +12,11 @@ def send_report(report_server_url, data_as_dict={}, headers={}):
     Makes a POST request to the report server. Ideally,
     the server should be able to upsert the old record
     because this POST request will be made every time the function is run
-    :param report_server_url:
-    :return:
     """
     response = requests.post(report_server_url, data=json.dumps(data_as_dict),
                              headers={'Content-Type': 'application/json', **headers})
-    if response.ok:
-        exit(0)
-    raise Exception('Sending report failed. \nresponse:\n %s' % response.reason)
+    if not response.ok:
+        raise requests.exceptions.HTTPError('Sending report failed. \nresponse:\n %s' % response.reason)
 
 
 def external_function_monitor(report_server_url, headers={}, **data):
@@ -26,9 +26,9 @@ def external_function_monitor(report_server_url, headers={}, **data):
             function_name = function.__name__
             host_name = socket.gethostname()
             report_data = {'function_name': function_name, 'host_name': host_name, **data}
-            send_report(report_server_url, data_as_dict=report_data)
-            # reporting = Process(target=send_report, args=(report_server_url,),
-            #                     kwargs={'data_as_dict': report_data, 'headers': headers})
+            # send_report(report_server_url, data_as_dict=report_data)
+            reporting = multiprocessing.Process(target=send_report, args=(report_server_url,),
+                                                kwargs={'data_as_dict': report_data, 'headers': headers})
             reporting.start()
             return function(*args, **kwargs)
 
--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 import unittest
+import socket
+import json
+import requests
+import multiprocessing
 from unittest import mock
 
 from main import external_function_monitor
 
 PASSING_URL = 'http://passing-url.com/test.json'
 NON_EXISTENT_URL = 'http://passing-url-does-not-exist.com/anothertest.json'
+EXTRA_DATA_TO_SEND = {'interval': 6, 'foo': 'bar'}
 
 
 def mocked_requests_post(*args, **kwargs):
@@ -20,10 +25,17 @@ def mocked_requests_post(*args, **kwargs):
         def json(self):
             return self.json_data
 
+        @property
         def ok(self):
             if self.status_code == 201:
                 return True
             return False
+
+        @property
+        def reason(self):
+            if self.status_code == 201:
+                return 'OK'
+            return 'Server Error'
 
     if args[0] == PASSING_URL:
         return MockResponse({"key1": "value1"}, 201)
@@ -32,8 +44,24 @@ def mocked_requests_post(*args, **kwargs):
 
     return MockResponse(None, 404)
 
+    #
+    # def mocked_multiprocessing_process_start(**kwargs):
+    #     """Mocks the Process class from multiprocessing"""
 
-@external_function_monitor(PASSING_URL, interval=6, foo='bar')
+    # return MockProcess(**kwargs)
+
+
+class MockProcess:
+    def __init__(self, target=lambda x: None, args=(), kwargs={}):
+        self.target = target
+        self.args = args
+        self.kwargs = kwargs
+
+    def start(self):
+        self.target(*self.args, **self.kwargs)
+
+
+@external_function_monitor(PASSING_URL, **EXTRA_DATA_TO_SEND)
 def simple_addition(first_number, second_number):
     """
     This function adds the first_number to the second_number
@@ -42,7 +70,7 @@ def simple_addition(first_number, second_number):
     return first_number + second_number
 
 
-@external_function_monitor(NON_EXISTENT_URL, interval=6, foo='bar')
+@external_function_monitor(NON_EXISTENT_URL, **EXTRA_DATA_TO_SEND)
 def simple_addition_with_misconfigured_decorator(first_number, second_number):
     """
     This function adds the first_number to the second_number
@@ -56,20 +84,37 @@ class TestFunctionExtMonitor(unittest.TestCase):
     Tests for the package function-ext-monitor
     """
 
+    def setUp(self) -> None:
+        """Initialize some variables"""
+        self.first_number = 6
+        self.second_number = 9
+
+    @mock.patch('multiprocessing.Process', new=MockProcess)
     @mock.patch('requests.post', side_effect=mocked_requests_post)
     def test_send_report(self, mock_post):
         """
         The decorator should be able to send a report every time
         the function is called
         """
-        first_number = 6
-        second_number = 9
+        simple_addition(self.first_number, self.second_number)
+        data_parameter_as_dict = {
+            'function_name': 'simple_addition',
+            'host_name': socket.gethostname(),
+            **EXTRA_DATA_TO_SEND
+        }
+        self.assertIn(mock.call(PASSING_URL,
+                                data=json.dumps(data_parameter_as_dict),
+                                headers={'Content-Type': 'application/json'}), mock_post.call_args_list)
 
-        # self.assertRaises(Exception, simple_addition_with_misconfigured_decorator(first_number, second_number))
-
-        simple_addition(first_number, second_number)
-
-        self.assertIn(mock.call(PASSING_URL), mock_post.call_args_list)
+    @mock.patch('multiprocessing.Process', new=MockProcess)
+    @mock.patch('requests.post', side_effect=mocked_requests_post)
+    def test_failed_server_response(self, mock_post):
+        """
+        The decorator should raise an exception if the request
+        to the reporting server fails
+        """
+        self.assertRaises(requests.exceptions.HTTPError,
+                          simple_addition_with_misconfigured_decorator, self.first_number, self.second_number)
 
 
 if __name__ == '__main__':
